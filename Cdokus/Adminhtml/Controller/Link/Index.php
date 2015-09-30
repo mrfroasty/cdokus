46ba1a7ca0a532f2ef24cad28dff3bd9f256a47d
OZ,Muhsin <o.z.muhsin@gmail.com>
2015-09-30 20:56:47 +0200

    #save

---
+++ Cdokus/Adminhtml/Controller/Link/Index.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Zanbytes\Cdokus\Controller\Adminhtml\Link;
+
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends \Magento\Backend\App\Action
+{
+    const ADMIN_RESOURCE = 'Zanbytes_Cdokus::cdokus_links';
+
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        die(__METHOD__.'#'.__LINE__);
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * Index action
+     *
+     * @return \Magento\Backend\Model\View\Result\Page
+     */
+    public function execute()
+    {
+        /** @var \Magento\Backend\Model\View\Result\Page $resultPage */
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu('Ashsmith_Blog::post');
+        $resultPage->addBreadcrumb(__('Blog Posts'), __('Blog Posts'));
+        $resultPage->addBreadcrumb(__('Manage Blog Posts'), __('Manage Blog Posts'));
+        $resultPage->getConfig()->getTitle()->prepend(__('Blog Posts'));
+
+        return $resultPage;
+    }
+}
+
